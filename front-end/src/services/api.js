649e81b57dde3669df36403aa72c452064167478
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-10-27 20:16:29 -0300

    fix: cors and try/catch

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -4,11 +4,19 @@ const LOGIN_ENDPOINT = 'http://localhost:3001/login';
 const REGISTER_ENDPOINT = 'http://localhost:3001/register';
 
 export async function login({ email, password }) {
-  const { data } = await axios.post(LOGIN_ENDPOINT, { email, password });
-  return data;
+  try {
+    const { data } = await axios.post(LOGIN_ENDPOINT, { email, password });
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
 
 export async function register({ email, password, name }) {
-  const { data } = await axios.post(REGISTER_ENDPOINT, { email, password, name });
-  return data;
+  try {
+    const { data } = await axios.post(REGISTER_ENDPOINT, { email, password, name });
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
