08721f239d73cdccbb8325faf6e097b8198148fd
Pedro Pereira <pedro.2108@hotmail.com>
2022-10-28 15:09:36 -0300

    fix: add user name validation for create new user

--- back-end/src/service/user.js
+++ back-end/src/service/user.js
@@ -12,7 +12,7 @@ const getUser = async (email, password) => user.findOne({
 });
 
 const create = async ({ email, password, name, role }) => {
-  const userAlreadyExist = await user.findOne({ where: { email } });
+  const userAlreadyExist = await user.findOne({ where: { email, name } });
 
   if (userAlreadyExist) {
     return null;
