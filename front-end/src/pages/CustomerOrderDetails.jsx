2954772efc801007885a03153d86fd3394065432
jatobabispo <dev.jatoba@gmail.com>
2022-11-01 15:11:30 -0300

    inicial commit: CustomerOrderDetails

---
+++ front-end/src/pages/CustomerOrderDetails.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+export default function CustomerOrderDetails() {
+  return (
+    <>
+
+    </>
+  );
+}
