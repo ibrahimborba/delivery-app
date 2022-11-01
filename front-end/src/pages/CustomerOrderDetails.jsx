3c75645162721439735615e4a900cf4fcd5c0fe4
jatobabispo <dev.jatoba@gmail.com>
2022-11-01 19:17:02 -0300

    feat: frontend table products - cutomer order details

--- front-end/src/pages/CustomerOrderDetails.jsx
+++ front-end/src/pages/CustomerOrderDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/HeaderProducts';
-import TableDetails from '../components/TableCheckout';
+import TableDetails from '../components/TableDatails';
 import { getSalesById } from '../services/api';
+import Button from '../components/Button';
 
 export default function CustomerOrderDetails() {
-  const [order, setOrder] = useState([]);
+  const [order, setOrder] = useState({ products: [] });
 
   const { id } = useParams();
 
@@ -20,11 +21,36 @@ export default function CustomerOrderDetails() {
   }, []);
 
   console.log(order);
+  const formateDate = (date) => {
+    const lastIndex = 10;
+    if (date) {
+      const result = date.substring(0, lastIndex);
+      const dateFormated = result.split('-').reverse().join('/');
+      return dateFormated;
+    }
+  };
+
+  const dataTestId = 'customer_order_details__element-order-';
   return (
     <>
       <Header />
       <h1>Customer Order Details</h1>
-      <TableDetails />
+      <span data-testid={ `${dataTestId}details-label-delivery-status${order.id}` }>
+        {order.status}
+      </span>
+      <span data-testid={ `${dataTestId}details-label-order-id` }>
+        {order.id}
+      </span>
+      <span data-testid={ `${dataTestId}details-label-order-date` }>
+        {formateDate(order.saleDate)}
+      </span>
+      <Button
+        dataTestId="customer_order_details__button-delivery-check"
+        type="button"
+        name="delivered"
+        text="Marcar Como Entregue"
+      />
+      <TableDetails orderById={ order.products } />
     </>
   );
 }
