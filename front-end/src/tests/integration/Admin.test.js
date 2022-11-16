d5ea433f279b481533b54d8fdf3dd19f277d2a60
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-11-16 18:50:51 -0300

    test(frontend): admin remove user test

--- front-end/src/tests/integration/Admin.test.js
+++ front-end/src/tests/integration/Admin.test.js
@@ -10,6 +10,10 @@ import { users } from '../mocks/apiResult';
 
 jest.mock('axios');
 
+const NAME = 'Nome Sobrenome';
+const EMAIL = 'nome@email.com';
+const PASSWORD = '%nome&sobrenome%';
+
 describe('Admin page', () => {
   describe('Render', () => {
     afterEach(() => jest.clearAllMocks());
@@ -43,6 +47,93 @@ describe('Admin page', () => {
       expect(userEmail[0]).toBeInTheDocument();
       expect(userRole[0]).toBeInTheDocument();
       expect(userRemoveBtn[0]).toBeInTheDocument();
+    })
+  })
+
+  describe('Behavior', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('Should disable register button if one input is not filled', async () => {
+      axios.get.mockResolvedValue({ data: users });
+      act(() => {
+        renderWithRouterContext(<Admin />, { loggedUser: administrator });
+      })
+      
+      const nameInput = screen.getByLabelText(/Nome/i);
+      const emailInput = screen.getByLabelText(/Email/i);
+      const passwordInput = screen.getByLabelText(/Senha/i);
+      const registerUserBtn = screen.getByRole('button', { name: /Cadastrar/i });
+
+      userEvent.type(nameInput, NAME);
+      userEvent.type(emailInput, EMAIL);
+
+      expect(registerUserBtn).toBeInTheDocument();
+      expect(registerUserBtn).toBeDisabled();
+    })
+
+    it('Should enable register button if all inputs are filled', async () => {
+      axios.get.mockResolvedValue({ data: users });
+      act(() => {
+        renderWithRouterContext(<Admin />, { loggedUser: administrator });
+      })
+      
+      const nameInput = screen.getByLabelText(/Nome/i);
+      const emailInput = screen.getByLabelText(/Email/i);
+      const passwordInput = screen.getByLabelText(/Senha/i);
+      const registerUserBtn = screen.getByRole('button', { name: /Cadastrar/i });
+
+      userEvent.type(nameInput, NAME);
+      userEvent.type(emailInput, EMAIL);
+      userEvent.type(passwordInput, PASSWORD);
+
+      expect(registerUserBtn).toBeInTheDocument();
+      expect(registerUserBtn).not.toBeDisabled();
+    })
+  })
+
+  describe('Delete user', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('Should remove user on remove button click', async () => {
+      axios.get.mockResolvedValue({ data: users });
+      axios.delete.mockResolvedValue({ data: users[0] });
+
+      act(() => {
+        renderWithRouterContext(<Admin />, { loggedUser: administrator });
+      })
+      
+      const userRemoveBtn = await screen.findAllByTestId(/admin_manage__element-user-table-remove/i);
+
+      act(() => {
+        userEvent.click(userRemoveBtn[0]);
+      })
+
+      expect(userRemoveBtn).toHaveLength(2);
+    })
+  })
+
+  describe('Register user', () => {
+    afterEach(() => jest.clearAllMocks());
+
+    it('Should remove user on remove button click', async () => {
+      axios.post.mockResolvedValue({ data: users[0] });
+
+      act(() => {
+        renderWithRouterContext(<Admin />, { loggedUser: administrator });
+      })
+      
+      const nameInput = screen.getByLabelText(/Nome/i);
+      const emailInput = screen.getByLabelText(/Email/i);
+      const passwordInput = screen.getByLabelText(/Senha/i);
+      const registerUserBtn = screen.getByRole('button', { name: /Cadastrar/i });
+
+      userEvent.type(nameInput, NAME);
+      userEvent.type(emailInput, EMAIL);
+      userEvent.type(passwordInput, PASSWORD);
+
+      act(() => {
+        userEvent.click(registerUserBtn);
+      })
     })
   })
 })
